abb213435a7dd9edff505bb66e07671ec7ff07e2
Grace Abike <mara234216@gmail.com>
2023-01-10 17:58:47 +0100

    Update CalcLogic.js

--- src/components/CalcLogic.js
+++ src/components/CalcLogic.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CalcLogic extends React.PureComponent {
-  render() {
-    const { children } = this.props;
-    return (
-      <div className="calcLogic">
-        {children}
-      </div>
-    );
-  }
-}
+const CalcLogic = ({ children }) => (
+  <div className="calcLogic">
+    {children}
+  </div>
+);
 
 CalcLogic.propTypes = {
   children: PropTypes.node.isRequired,
